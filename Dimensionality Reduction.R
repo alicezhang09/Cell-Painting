7048ae58327f245eefbb9bf7dd8be4f5d4ac8100
Alice Zhang <alicezhang09@hotmail.com>
2020-06-25 09:30:53 -0700

    test

--- Dimensionality Reduction.R
+++ Dimensionality Reduction.R
@@ -8,6 +8,11 @@ library(purrr)
 library(lineup)
 library(ppcor)
 library(caret)
+library(philentropy)
+library(scales)
+library(spade)
+library(devtools)
+devtools::install_github("nolanlab/Rclusterpp")
 
 #function to import dataframes and remove rows and columns with mostly empty cells
 #Parameter: list of file names
@@ -47,10 +52,12 @@ metadata<-function(df,main, x, y){
   return(df)
 }
 
+
 #Shapiro- Wilks function
 #Parameters: dataframe, threshold
 #Returns: dataframe with only columns with W<threshold
 shapiro<-function(df, threshold){
+  df<- subset(df, select=-c(Metadata_PositionX,Metadata_Series, Metadata_Site))
   df<- df[ ,- as.numeric(which(apply(df, 2, var) == 0))]
   lshap <- lapply(df, shapiro.test)
   lres <- sapply(lshap, `[`, c("statistic","p.value"))
@@ -68,24 +75,6 @@ average<-function(longer,shorter){
   average<-average%>% slice(rep(1, each=nrow(longer)-nrow(shorter)))
   shorter2<-rbind(average, shorter)
   return(shorter2)
-}
-
-
-#function that produces heatmap of correlation matrix between two dataframes 
-#Parameters: two datframes with equal dimensions
-#Returns: heatmap of correlation matrix (Method: Pearson)
-heatmap_function<-function(x,y){
-  corr<-cor(x,y)
-  corr<-corr[rowSums(is.na(corr))<ncol(corr)*0.5,colSums(is.na(corr))<nrow(corr)*0.5]
-  heatmap1<<- heatmap.2(corr, scale = "none", dendrogram='none', Rowv=TRUE, Colv=TRUE, 
-                        col = colorpanel(1000, "red", "white", "red"), trace = "none", density.info = "none", 
-                        xlab=x, ylab=y, main= "Correlation matrix heatmap", margins = c(12,12))
-}
-
-heatmap1<-function(x){
-  heatmap<<-heatmap.2(x, scale = "none", dendrogram='none', Rowv=TRUE, Colv=TRUE, 
-                      col = colorpanel(1000, "red", "white", "red"), trace = "none", density.info = "none", 
-                      xlab=x, ylab=x, main= "Correlation matrix heatmap", margins = c(12,12))
 }
 
 #import feature dataframes and remove rows and columns with more than half empty cells
@@ -131,9 +120,77 @@ IWP_nuclei<-metadata(IWP_nuclei, nuclei, 6, 7)
 IWP_cytoplasm<-metadata(IWP_cytoplasm, cytoplasm, 6, 7)
 
 
-#running Shapiro-Wilks test
-Shapiro_control_nuclei<-shapiro(control_nuclei,0.1)
+#separate dataframes based on site
 
+cells_site10<-cells%>%
+  filter(ImageNumber==1)
+cells_site11<-cells%>%
+  filter(ImageNumber==2)
+cells_site12<-cells%>%
+  filter(ImageNumber==3)
+cells_site13<-cells%>%
+  filter(ImageNumber==4)
+cells_site14<-cells%>%
+  filter(ImageNumber==5)
+cells_site15<-cells%>%
+  filter(ImageNumber==6)
+cells_site16<-cells%>%
+  filter(ImageNumber==7)
+cells_site9<-cells%>%
+  filter(ImageNumber==8)
+
+# compute mean, median, variance for each cell feature
+
+cells_mean<- aggregate(.~ImageNumber, FUN=mean, data=cells)
+cells_median<- aggregate(.~ImageNumber, FUN=median, data=cells)
+cells_var<- aggregate(.~ImageNumber, FUN=var, data=cells)
+mean_var<-as.data.frame(sapply(cells_mean, var))
+
+ces <- data.frame(lapply(cells_median[,-1], function(x) (x-min(x))/(max(x) - min(x)) * 10))
+ces<-cbind(cells_median[,1], ces)
+rownames(ces) <- ces[,1]
+heatmap.2(as.matrix(ces), scale = "none", dendrogram='none', Rowv=FALSE, Colv=FALSE, 
+          col = colorpanel(1000, "white", "blue"), trace = "none", density.info = "none", 
+          xlab="median profile", ylab="condition", main= "Median Profiles", margins = c(12,5))
+
+cells_mean<- aggregate(.~ImageNumber, FUN=mean, data=cells)
+cells_median<- aggregate(.~Metadata_PositionY, FUN=median, data=cells)
+cells_var<- aggregate(.~ImageNumber, FUN=var, data=cells)
+
+#create histograms of distributions for a few features
+Cells_text<-cells$Texture_Correlation_Actin_10_00
+hist(Cells_text, breaks=100)
+cells_solidity<-cells$AreaShape_Solidity
+hist(cells_solidity, breaks=100)
+nuclei_neighbors<-nuclei$Neighbors_NumberOfNeighbors_5
+hist(nuclei_neighbors, breaks=10)
+nuclei_intensity<-nuclei$Intensity_MinIntensity_Actin
+hist(nuclei_intensity, breaks=200)
+
+#create distance matrix and heatmap of cell feature means
+dists <- dist(cells_mean)
+dists<- as.matrix(dists)
+dists<-round(dists, 2)
+heatmap.2(dists, Rowv=FALSE, Colv=FALSE, col = colorpanel(1000, "red", "white", "blue"),  margins=c(3,3), cellnote=dists,
+          notecex=1.0, notecol="black",na.color=par("bg"), main= "Site Distance Heatmap")
+
+#create correlation matrix and heatmap of cell feature means
+corr<-cor(t(cells_mean))
+corr<-round(as.matrix(corr),6)
+heatmap.2(corr, Rowv=FALSE, Colv=FALSE, col = colorpanel(1000, "red", "white", "blue"),  margins=c(3,3), cellnote=corr,         
+          notecex=1.0, notecol="black",na.color=par("bg"), main= "Site Correlation Heatmap")
+
+
+#running Shapiro-Wilks test
+Shapiro_control_nuclei<-as.matrix(shapiro(control_nuclei,0.1))
+shapiro_control_cells<-as.matrix(shapiro(control_cells, 0.1))
+shapiro_control_cytoplasm<-as.matrix(shapiro(control_cyto, 0.1))
+
+shapiro_cells<-as.matrix(shapiro(cells,0.2))
+shapiro_nuclei<-as.matrix(shapiro(nuclei, 0.2))
+shapiro_cytoplasm<- shapiro(cytoplasm, 0.2)
+
+write.csv(shapiro_control_cytoplasm,"C:\\Users\\xinli\\Desktop\\shapiro_control_cytoplasm.csv", row.names = TRUE)
 
 # to create a heatmap of a correlation matrix: 
 y<- average(Rock_cells, DMSO_cells)
@@ -202,6 +259,20 @@ cell_nucleoli_features<- cells[indx]
 indx<- grepl('ER',colnames(cells))
 cell_ER_features<-cells[indx]
 
+indx<- grepl('mito',colnames(nuclei))
+nuc_mito_features<-nuclei[indx]
+indx<-grepl('DNA', colnames(nuclei))
+nuc_DNA_features<- nuclei[indx]
+indx<- grepl('Actin',colnames(nuclei))
+nuc_actin_features<-nuclei[indx]
+indx<-grepl('Nucleoli', colnames(nuclei))
+nuc_nucleoli_features<- nuclei[indx]
+indx<- grepl('ER',colnames(nuclei))
+nuc_ER_features<-nuclei[indx]
+
+nuc<-as.data.frame(colnames(nuclei))
+write.csv(nuc,"C:\\Users\\xinli\\Desktop\\Features.csv", row.names = FALSE)
+
 #construct correlation matrices and heatmaps to compare different wavelengths
 corr<-cor(cell_mito_features)
 corr<-corr[rowSums(is.na(corr))<ncol(corr)*0.5,colSums(is.na(corr))<nrow(corr)*0.5]
@@ -250,8 +321,14 @@ control_cells_matrix<-matrix(as.numeric(unlist(control_cells)),nrow=nrow(control
 DMSO_nuclei_matrix<-matrix(as.numeric(unlist(DMSO_nuclei)),nrow=nrow(DMSO_nuclei))
 control_nuclei_matrix<-matrix(as.numeric(unlist(control_nuclei)),nrow=nrow(control_nuclei))
 
+ex<-as.data.frame(colnames(cells))
+write.csv(ex,"C:\\Users\\xinli\\Desktop\\Features.csv", row.names = FALSE)
+
+
+
 dists <- pdist(t(DMSO_cells), t(control_cells))
 dists<- as.matrix(dists)
 heatmap.2(dists, Colv=NA, Rowv=NA, col=cm.colors(256), scale="row", margins=c(3,3), xlab= "DMSO", ylab="Control",
           main= "Feature-Feature Correlation Heatmap",xlab="DMSO", ylab="control”)
+          
           
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Cell-Painting
-This image analysis pipeline was developed to differentiate heart cells during their transition from iPSCs to cardiomyocytes. The goal was to extract as many features as possible from a set of 4000 images taken of a 96 well plate. 
+This image analysis pipeline was developed to differentiate heart cells during their transition from iPSCs to cardiomyocytes. The goal was to extract as many features as possible from a set of 4000 images taken of a 96 well plate. test
+
